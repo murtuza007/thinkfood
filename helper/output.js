63aa52a9d244fd0123dfc3834cc2314f84210fc5
Murtuza Kothawala <murtuzakothawala@qnap.com>
2016-06-15 21:28:04 +0800

    User details service implemented

--- helper/output.js
+++ helper/output.js
@@ -12,17 +12,20 @@ module.exports = {
 			'11001': 'Please provide a valid username and password',
 			'11002': 'Please provide a valid username',
 			'11003': 'Please provide a valid user id',
+
 			
 
 
 			//failure code 12000
 			'12001': 'No user present',
 			'12002': 'Username already exists',
+			'11003': 'User with this details not present',
 
 			//Success code 13000
 			'13001': 'User login succes',
 			'13002': 'Username not present',
-			'13003': 'User registered successfuly'
+			'13003': 'User registered successfuly',
+			'13004': 'User details'
 
 		};
 		return code[statusCode];
--- routes/user/index.js
+++ routes/user/index.js
@@ -9,56 +9,8 @@ var pool = dbConnector.getConnectParams();
 var outputHelper = require('../../helper/output');
 
 
-router.route('/:user_id')
-//get the user profile
-.get(function(req,res,next){
-	var userId = req.query.user_id
-	console.log(userId);
-	if (userId) {
-		pool.getConnection(function(err,connection){
-	        if (err) {	        	
-				res.json(outputHelper.output(10001));			
-	        }
-
-	        var query = "SELECT	u.* ,
-	        				sum(case when uf.user_id = u.id then 1 else 0 end) as ifollow,
-	        				sum(case when uf.follower_id = u.id then 1 else 0 end) as myfollow
-        				FROM 	
-        					user u
-        				left join 
-        					user_follower uf 
-        					on 
-        						(uf.follower_id = u.id OR uf.user_id  = u.id)
-    						where
-    							u.id = ?;
-						";	        
-	        connection.query(query,function(err,rows){
-	            connection.release();
-	            if(err) {	            	
-	                res.json(outputHelper.output(10001));
-	            } else {	            	
-	            	if(rows.length > 0) {
-	            		res.json(outputHelper.output(13001), rows[0]);
-	            	} else {
-	            		res.json(outputHelper.output(12001));
-	            	}
-	            }
-	        });
-
-	        connection.on('error', function(err) {      
-				res.json(outputHelper.output(10001));	              
-	        });
-	  	});
-  	} else {
-  		res.json(outputHelper.output(11003));
-  	}
-
-
-	res.json(outputHelper.output(11002));
-});
-
 router.route('/checkUserName')
-.post(function(req, res, next) {	
+.post(function(req, res, next) {		
 	var userHelper = require('../../helper/user_general');
 	var username = req.body.username;	
 	if (username) {		
@@ -75,5 +27,48 @@ router.route('/checkUserName')
 	
 });
 
+router.route('/:user_id')
+//get the user profile
+.get(function(req,res,next){
+	var userId = req.params.user_id	
+	if (userId) {
+		pool.getConnection(function(err,connection){
+	        if (err) {	        	
+				res.json(outputHelper.output(10001));	
+				return;		
+	        }
+
+	        var query = "SELECT	u.* , sum(case when uf.user_id = u.id then 1 else 0 end) as i_follow, sum(case when uf.follower_id = u.id then 1 else 0 end) as my_follow"
+	        			+ " FROM 	"
+    					+	"	user u"
+    					+	" left join "
+        				+	" user_follower uf "
+        				+	" on "
+        				+	"	(uf.follower_id = u.id OR uf.user_id  = u.id) "
+    					+	" where "
+    					+	"	u.id = ?; ";
+						
+	        connection.query(query, [userId], function(err,rows){
+	            connection.release();
+	            if(err) {	            	
+	                res.json(outputHelper.output(10001));
+	            } else {	            	
+	            	if(rows.length > 0) {
+	            		res.json(outputHelper.output(13004, rows[0]));
+	            	} else {
+	            		res.json(outputHelper.output(12001));
+	            	}
+	            }
+	        });
+
+	        connection.on('error', function(err) {      
+				res.json(outputHelper.output(10001));
+				return;	              
+	        });
+	  	});
+  	} else {
+  		res.json(outputHelper.output(11003));
+  	}
+});
 
 module.exports= router;
